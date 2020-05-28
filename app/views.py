31e4bc13e0f6d88978186e8d833e269bd3169b56
haruyasu <harukun2002@gmail.com>
2020-05-28 18:13:44 +0900

    :rocket: 検索まで

--- app/views.py
+++ app/views.py
@@ -1,7 +1,45 @@
-from django.views.generic import (TemplateView)
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import View
+from django.shortcuts import render
+import json
+import requests
+
+SEARCH_URL = 'https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?format=json&applicationId=1002196771843734307'
 
 
-class IndexView(LoginRequiredMixin, TemplateView):
-    template_name = "app/index.html"
-    login_url = '/accounts/login/'
+def get_data(title):
+    params = {
+        'title': title
+    }
+
+    api = requests.get(SEARCH_URL, params=params).text
+    result = json.loads(api)
+
+    book_data = []
+    for i in result['Items']:
+        item = i['Item']
+        title = item['title']
+        image = item['largeImageUrl']
+        query = {
+            'title': title,
+            'image': image
+        }
+        book_data.append(query)
+    return book_data
+
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        book_data = []
+
+        return render(request, 'app/index.html', {
+            'book_data': book_data
+        })
+
+    def post(self, request, *args, **kwargs):
+        search = request.POST.get('search')
+
+        book_data = get_data(search)
+
+        return render(request, 'app/index.html', {
+            'book_data': book_data
+        })
